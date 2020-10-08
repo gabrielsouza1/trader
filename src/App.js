297b032570f1b478b2efd92aa026c1b16892ce4d
Gabriel Souza <contatogabrielsouza23@gmail.com>
2020-10-08 18:22:38 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Style.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  color:{
+    backgroundColor: '#FFFFFF',
+    color: '#333333',
+  },
+}));
 
 function App() {
+    const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static" className={classes.color}>
+        <Toolbar variant="dense" >  
+          <Typography variant="h6" color="inherit" className={classes.title}>
+            TRADER
+          </Typography>
+          <Button color="inherit">Entrar</Button>
+          <Button color="inherit">Cadastre-se</Button>
+        </Toolbar>
+      </AppBar>
+
     </div>
   );
 }
